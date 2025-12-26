03091d73bee748b49075bba0c4fea6c06920fbf1
chaewookim <chaewookim2877@gmail.com>
2025-12-26 16:08:57 +0900

    [feat] #13 Transaction Entity 수정
    사용 금액인 amount의 타입을 BigDecimal로 변경

--- src/main/java/com/chaewookim/accountbookformoms/domain/transaction/entity/Transaction.java
+++ src/main/java/com/chaewookim/accountbookformoms/domain/transaction/entity/Transaction.java
@@ -4,13 +4,23 @@ import com.chaewookim.accountbookformoms.domain.transaction.enums.TransactionTyp
 import com.chaewookim.accountbookformoms.global.entity.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Entity
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
 public class Transaction extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -31,10 +41,11 @@ public class Transaction extends BaseEntity {
     @Column(nullable = false)
     private Long categoryId;
 
-    @Column(nullable = false)
-    private Long amount;
+    @Column(nullable = false, precision = 12, scale = 4)
+    private BigDecimal amount;
 
     @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
     private TransactionTypeEnum type;
 
     @Column(nullable = false)
